761261ba5a3a59af8429d907aaeefc9dc09d88b2
Leo-Gorriarena <115600166+Leo-Gorriarena@users.noreply.github.com>
2022-10-24 21:51:09 -0300

    Add index.html and script.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rock_Paper_Scissors
+
+https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
---
+++ script.js
@@ -0,0 +1,71 @@
+let humanChoice;
+let computerChoice = "";
+
+const getComputerChoice = function () {
+  let random = Math.floor(Math.random() * 3) + 1;
+  if (random === 1) {
+    computerChoice = "rock";
+  } else if (random === 2) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+  return computerChoice;
+};
+
+const playRound = function (playerSelection, computerSelection) {
+  let humanPlayer = playerSelection.toLowerCase();
+
+  if (humanPlayer === "rock" && computerSelection === "scissors") {
+    console.log("You Win! Rock beats Scissors");
+    return 1;
+  } else if (humanPlayer === "paper" && computerSelection === "rock") {
+    console.log("You Win! Paper beats Rock");
+    return 1;
+  } else if (humanPlayer === "scissors" && computerSelection === "paper") {
+    console.log("You Win! Scissors beats Paper");
+    return 1;
+  } else if (humanPlayer === computerSelection) {
+    console.log(
+      `It's a Tie! ${computerSelection.toUpperCase()} === ${humanPlayer.toUpperCase()}`
+    );
+    return 0;
+  } else {
+    console.log(
+      `You Lose! ${computerSelection.toUpperCase()} beat ${humanPlayer.toUpperCase()}`
+    );
+    return 2;
+  }
+};
+
+/* Write a NEW function called game(). Call the playRound function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end. */
+
+const game = function () {
+  let roundResult;
+  let humanScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    humanChoice = prompt(
+      "Choose your weapon: Rock | Paper | Scissors"
+    ).toLowerCase();
+
+    roundResult = playRound(humanChoice, getComputerChoice());
+
+    if (roundResult === 1) {
+      humanScore++;
+    } else if (roundResult === 2) {
+      computerScore++;
+    }
+  }
+
+  if (humanScore === computerScore) {
+    console.log("It is a tie!!!");
+  } else if (humanScore > computerScore) {
+    console.log(`You Win!!! ${humanScore}-${computerScore}`);
+  } else {
+    console.log(`COMPUTER WINS!!! ${computerScore}-${humanScore}`);
+  }
+};
+
+game();
